c9561d269d60848dd63b06ef5edfb1e562cefecb
Felipe Machado <felipemmachado28@gmail.com>
2022-12-14 11:35:27 -0300

    criacao

--- src/infra/database/repositories/prisma-notifications-repository.ts
+++ src/infra/database/repositories/prisma-notifications-repository.ts
@@ -6,6 +6,14 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
+    console.log(notification, {
+      id: notification.id,
+      content: notification.content.value,
+      category: notification.category,
+      recipientId: notification.recipientId,
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    });
     await this.prismaService.notification.create({
       data: {
         id: notification.id,
--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,14 +1,25 @@
 import { Controller, Post } from '@nestjs/common';
-import { PrismaService } from '../../database/prisma/prisma.service';
+
 import { Body } from '@nestjs/common/decorators';
 import { CreateNotificationDTO } from '../dtos/create-notification-dto';
 import { ApiTags } from '@nestjs/swagger';
+import { SendNotification } from 'src/application/use-cases/send-notification';
 
 @Controller('notifications')
 @ApiTags('Notifications')
 export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
   @Post()
   async create(@Body() body: CreateNotificationDTO) {
-    console.log(body);
+    const { recipientId, category, content } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      category,
+      content,
+    });
+
+    return { notification };
   }
 }
