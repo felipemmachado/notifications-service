b45320a7343ac08c5b454a2d1299e74ae718d48d
“Felipe <felipe.machado@localiza.com>
2022-12-15 17:28:01 -0300

    cancel notification

--- src/application/repositories/notifications-repository.ts
+++ src/application/repositories/notifications-repository.ts
@@ -2,4 +2,6 @@ import { Notification } from '@domain/entities/notification/notification';
 
 export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract save(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
 }
