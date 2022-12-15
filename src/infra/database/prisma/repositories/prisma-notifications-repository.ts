bc115ef25985ceebb936d731db96cc48b92130f3
“Felipe <felipe.machado@localiza.com>
2022-12-15 16:44:03 -0300

    mapper

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -2,21 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { NotificationsRepository } from '@application/repositories/notifications-repository';
 import { Notification } from '@domain/entities/notification/notification';
 import { PrismaService } from '../prisma.service';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prisma: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
     await this.prisma.notification.create({
-      data: {
-        id: notification.id,
-        content: notification.content.value,
-        category: notification.category,
-        recipientId: notification.recipientId,
-        readAt: notification.readAt,
-        createdAt: notification.createdAt,
-      },
+      data: raw,
     });
   }
 }
